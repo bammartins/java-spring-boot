14705abb4bcb7f3ef0aa668147f69d86ec2c8f12
Bruno Araujo Martins <bruno.martins@trusthub.com.br>
2019-05-22 11:57:54 -0300

    Aplicando Validacao customizada para email duplicado no banco

--- src/main/java/com/bmartins/modelagem/repositories/ClienteRepository.java
+++ src/main/java/com/bmartins/modelagem/repositories/ClienteRepository.java
@@ -3,6 +3,7 @@ package com.bmartins.modelagem.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bmartins.modelagem.domain.Cliente;
 
@@ -10,5 +11,7 @@ import com.bmartins.modelagem.domain.Cliente;
 //Camada que disponibiliza as transacoes com bd
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/bmartins/modelagem/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/bmartins/modelagem/services/validation/ClienteInsertValidator.java
@@ -2,16 +2,25 @@ package com.bmartins.modelagem.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bmartins.modelagem.domain.Cliente;
 import com.bmartins.modelagem.domain.enums.TipoCliente;
 import com.bmartins.modelagem.dto.ClienteNewDTO;
+import com.bmartins.modelagem.repositories.ClienteRepository;
 import com.bmartins.modelagem.resources.exception.FieldMessage;
 import com.bmartins.modelagem.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -20,7 +29,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public boolean isValid(ClienteNewDTO cli, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 		
+		Cliente cliByEmail = clienteRepository.findByEmail(cli.getEmail());
 		
+		if (cliByEmail != null && cliByEmail.getEmail() != null) {
+			list.add(new FieldMessage("email", "Email indisponível"));
+		}
 		
 		if (cli.getTipo().equals(TipoCliente.PESSOAFISICA.getId()) && !BR.isValidSsn(cli.getDocumento())) {
 			list.add(new FieldMessage("documento", "CPF inválido"));
